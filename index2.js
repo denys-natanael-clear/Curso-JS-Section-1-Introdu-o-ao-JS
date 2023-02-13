00c9075e360809c0b4c568a1fe7c2a5e2020af3a
denys-natanael-clear <denys.natanael.clear@gmail.com>
2023-02-13 12:35:44 -0300

    correção: inserção do operador + que estava faltando na linha 30 após a variável n

--- index2.js
+++ index2.js
@@ -27,6 +27,6 @@
     for (let i = 0; i <= 10; i++){
 
         console.log(`${i} x ${n} = ${i*n}`)
-        console.log(i + " X " + n " = " + (i * n))
+        console.log(i + " X " + n + " = " + (i * n))
 
     }
