0dbe8a4b2991aeb6c2f70575f82b777faffdab50
denys-natanael-clear <denys.natanael.clear@gmail.com>
2023-02-13 12:32:43 -0300

    Introdução aos conceitos básico de javascript como: console, variáveis, operadores, condicionais e laços de repetição

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
 // INTRODUÇÃO AO JS - BÁSICO 1
+
+// Manipulando o console
+console.log('Olá Mundo!')
+
+// Variáveis
+
+var OlaMundo = "Hello world!d"
+
+console.log(OlaMundo)
+console.log(OlaMundo)
+console.log(OlaMundo)
+
+// Operadores 
+
+    // atribuição
+    let a = 10
+    let b = "100"
+
+    //comparação valor
+    console.log(a == b)
+
+    //comparação de valor e igualdade
+    console.log(a === b)
+
+    //diferente no valor
+    console.log(a != b)
+
+    //diferente on valor e igualdade
+    console.log(a !== b)
+
+    //operador &&
+    console.log(a == b && typeof b == 'string')
+
+    //operador ||
+    console.log(a == b || typeof b == 'string')
+
+// operador de condiçãoes if e else
+
+    let cor = 'amarelo'
+
+    if (cor === 'verde'){
+        console.log('siga')
+    }
+
+    else if (cor === 'amarelo'){
+        console.log('atenção')
+    }else {
+        console.log('pare')
+    }
+
+
+
+    
+    
---
+++ index2.js
@@ -0,0 +1,32 @@
+// estrutura de controle switch
+
+    let cor = 'vermelho'
+    
+    switch(cor){
+
+        case "verde":
+        console.log('siga')
+        break
+
+        case "amarelo":
+        console.log('atenção')
+        break
+
+        case "vermelho":
+        console.log('pare')
+        break
+
+        default:
+            console.log('não sei')
+    }
+
+// laço de repetição for
+
+    let n = 5
+
+    for (let i = 0; i <= 10; i++){
+
+        console.log(`${i} x ${n} = ${i*n}`)
+        console.log(i + " X " + n " = " + (i * n))
+
+    }
